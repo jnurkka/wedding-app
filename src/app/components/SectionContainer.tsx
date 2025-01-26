b9e8fbc608009cb6042abcaaf05ce87a531e5c85
Jaakko Nurkka <jaakko@cliniserve.de>
2025-01-26 21:36:29 +0100

    feat: improve mobile layout

--- src/app/components/SectionContainer.tsx
+++ src/app/components/SectionContainer.tsx
@@ -5,7 +5,7 @@ export const SectionContainer: React.FC<{
   bgColor: string;
   bgImage?: string;
 }> = ({ children, bgColor, bgImage }) => {
-  const className = `relative min-h-screen w-screen items-center snap-start flex flex-col justify-start md:justify-center px-6 md:px-12 lg:px-16 py-8`;
+  const className = `relative min-h-screen w-screen items-center snap-start flex flex-col justify-center px-6 md:px-12 lg:px-16 py-8`;
   const style = {
     backgroundColor: bgColor,
     ...(bgImage
