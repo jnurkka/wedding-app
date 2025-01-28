73f5011b6e2ba1d0d6d851a53250c22354217513
Jaakko Nurkka <jaakko@cliniserve.de>
2025-01-28 22:52:28 +0100

    chore: lint

--- src/app/components/sections/Response.tsx
+++ src/app/components/sections/Response.tsx
@@ -36,7 +36,9 @@ export const Response = ({
         comment: data.get("comment") as string,
       });
       console.log("Status:", status);
-      setSubmitMessage("Your response has been saved successfully! You can come back here to update it anytime.");
+      setSubmitMessage(
+        "Your response has been saved successfully! You can come back here to update it anytime.",
+      );
     } catch (error) {
       console.error("Error submitting registration:", error);
       setSubmitMessage("An error occurred. Please try again.");
@@ -58,106 +60,114 @@ export const Response = ({
         <Card>
           <CardTitle value="Your personal response" />
           <form onSubmit={handleSubmit} className="space-y-6 p-6">
-          <span className="block text-[#4A4238] text-sm mb-2 text-center md:text-left">
-                    Your email: {email}
-                  </span>
-              <div className="grid grid-cols-1 gap-4">
-                <label className="block text-[#4A4238]">
-                  <span className="block mb-2 font-semibold">Your name</span>
-                  <input
-                    type="text"
-                    name="respondentName"
-                    defaultValue={registration?.name || ""}
-                    className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
-                    placeholder="Enter your name"
-                    required
-                  />
-                </label>
-                <label className="block text-[#4A4238]">
-                  <span className="block mb-2 font-semibold">No of people (Friday)</span>
-                  <input
-                    type="number"
-                    min="0"
-                    max="5"
-                    name="people_fr"
-                    defaultValue={registration?.people_fr}
-                    className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
-                    placeholder="How many will you be on Friday?"
-                  />
-                  </label>
-                  <label className="block text-[#4A4238]">
-                    <span className="block mb-2 font-semibold">No of people (Saturday)</span>
-                    <input
-                      type="number"
-                      min="0"
-                      max="5"
-                      name="people_sat"
-                      defaultValue={registration?.people_sat}
-                      className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
-                      placeholder="How many will you be on Saturday?"
-                    />
-                  </label>
-                <label className="flex items-center space-x-3 cursor-pointer group">
-                  <input
-                    type="checkbox"
-                    defaultChecked={registration?.staying_fr}
-                    name="staying_fr"
-                    className="form-checkbox text-[#4A4238] transition-all"
-                  />
-                  <span className="text-left text-[#4A4238] group-hover:text-[#4A4238]/80 transition-colors">
-                    Staying in the Acker Hotel on Friday
-                  </span>
-                </label>
-                <label className="flex items-center space-x-3 cursor-pointer group">
-                  <input
-                    type="checkbox"
-                    defaultChecked={registration?.staying_sat}
-                    name="staying_sat"
-                    className="form-checkbox text-[#4A4238] transition-all"
-                  />
-                  <span className="text-left text-[#4A4238] group-hover:text-[#4A4238]/80 transition-colors">
-                    Staying in the Acker Hotel on Saturday
-                  </span>
-                </label>
-              </div>
-                <label className="block text-[#4A4238]">
-                  <span className="block mb-2 font-semibold">Dietary Restrictions</span>
-                  <textarea
-                    name="diet"
-                    rows={2}
-                    defaultValue={registration?.diet || ""}
-                    className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
-                    placeholder="Dietary restrictions (allergies, intolerances, diets)"
-                  />
-                </label>
-                <label className="block text-[#4A4238]">
-                  <span className="block mb-2 font-semibold">Additional Comments</span>
-                  <textarea
-                    name="comment"
-                    defaultValue={registration?.comment || ""}
-                    rows={3}
-                    className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all resize-y"
-                    placeholder="Leave any comments or requests here"
-                  />
-                </label>
+            <span className="block text-[#4A4238] text-sm mb-2 text-center md:text-left">
+              Your email: {email}
+            </span>
+            <div className="grid grid-cols-1 gap-4">
+              <label className="block text-[#4A4238]">
+                <span className="block mb-2 font-semibold">Your name</span>
+                <input
+                  type="text"
+                  name="respondentName"
+                  defaultValue={registration?.name || ""}
+                  className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
+                  placeholder="Enter your name"
+                  required
+                />
+              </label>
+              <label className="block text-[#4A4238]">
+                <span className="block mb-2 font-semibold">
+                  No of people (Friday)
+                </span>
+                <input
+                  type="number"
+                  min="0"
+                  max="5"
+                  name="people_fr"
+                  defaultValue={registration?.people_fr}
+                  className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
+                  placeholder="How many will you be on Friday?"
+                />
+              </label>
+              <label className="block text-[#4A4238]">
+                <span className="block mb-2 font-semibold">
+                  No of people (Saturday)
+                </span>
+                <input
+                  type="number"
+                  min="0"
+                  max="5"
+                  name="people_sat"
+                  defaultValue={registration?.people_sat}
+                  className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
+                  placeholder="How many will you be on Saturday?"
+                />
+              </label>
+              <label className="flex items-center space-x-3 cursor-pointer group">
+                <input
+                  type="checkbox"
+                  defaultChecked={registration?.staying_fr}
+                  name="staying_fr"
+                  className="form-checkbox text-[#4A4238] transition-all"
+                />
+                <span className="text-left text-[#4A4238] group-hover:text-[#4A4238]/80 transition-colors">
+                  Staying in the Acker Hotel on Friday
+                </span>
+              </label>
+              <label className="flex items-center space-x-3 cursor-pointer group">
+                <input
+                  type="checkbox"
+                  defaultChecked={registration?.staying_sat}
+                  name="staying_sat"
+                  className="form-checkbox text-[#4A4238] transition-all"
+                />
+                <span className="text-left text-[#4A4238] group-hover:text-[#4A4238]/80 transition-colors">
+                  Staying in the Acker Hotel on Saturday
+                </span>
+              </label>
+            </div>
+            <label className="block text-[#4A4238]">
+              <span className="block mb-2 font-semibold">
+                Dietary Restrictions
+              </span>
+              <textarea
+                name="diet"
+                rows={2}
+                defaultValue={registration?.diet || ""}
+                className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all"
+                placeholder="Dietary restrictions (allergies, intolerances, diets)"
+              />
+            </label>
+            <label className="block text-[#4A4238]">
+              <span className="block mb-2 font-semibold">
+                Additional Comments
+              </span>
+              <textarea
+                name="comment"
+                defaultValue={registration?.comment || ""}
+                rows={3}
+                className="w-full px-3 py-2 bg-white border border-[#4A4238] rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#4A4238]/50 transition-all resize-y"
+                placeholder="Leave any comments or requests here"
+              />
+            </label>
 
-              <div className="text-center">
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="w-full max-w-xs mx-auto bg-[#4A4238] text-white px-6 py-3 rounded-lg hover:bg-opacity-80 transition-colors disabled:opacity-50 focus:outline-none focus:ring-2 focus:ring-[#4A4238] focus:ring-opacity-50"
-                >
-                  {isSubmitting ? "Submitting..." : "Submit RSVP"}
-                </button>
-              </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full max-w-xs mx-auto bg-[#4A4238] text-white px-6 py-3 rounded-lg hover:bg-opacity-80 transition-colors disabled:opacity-50 focus:outline-none focus:ring-2 focus:ring-[#4A4238] focus:ring-opacity-50"
+              >
+                {isSubmitting ? "Submitting..." : "Submit RSVP"}
+              </button>
+            </div>
 
-              {submitMessage && (
-                <p
-                  className={`mt-4 text-center ${submitMessage.includes("error") ? "text-red-500" : "text-green-500"}`}
-                >
-                  {submitMessage}
-                </p>
-              )}
+            {submitMessage && (
+              <p
+                className={`mt-4 text-center ${submitMessage.includes("error") ? "text-red-500" : "text-green-500"}`}
+              >
+                {submitMessage}
+              </p>
+            )}
           </form>
         </Card>
       </SectionContainer>
