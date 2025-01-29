d0b30b6fb7c0686492e655b5e625446ed9f2410b
Jaakko Nurkka <jaakko@cliniserve.de>
2025-01-29 11:09:52 +0100

    feat: use emoticons for vehicles

--- src/app/components/sections/TravelInfo.tsx
+++ src/app/components/sections/TravelInfo.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaCar, FaExternalLinkAlt, FaPlane, FaTrain } from "react-icons/fa";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import { SectionContainer } from "../SectionContainer";
 import { SectionTitle } from "../SectionTitle";
 import { Card, CardContainer, CardTitle } from "../Card";
@@ -9,13 +9,11 @@ const travelOptions = [
     origin: "Bielefeld",
     methods: [
       {
-        icon: <FaTrain className="text-[#4A4238] mr-2" />,
-        description: "Bielefeld to Neuburg (ca. 6-7h)",
+        description: "🚂 Bielefeld to Neuburg (ca. 6-7h)",
         link: "https://maps.app.goo.gl/xhzMtynWqJrVGpW89",
       },
       {
-        icon: <FaCar className="text-[#4A4238] mr-2" />,
-        description: "Bielefeld to Neuburg (ca. 5,5h)",
+        description: "🚗 Bielefeld to Neuburg (ca. 5,5h)",
         link: "https://maps.app.goo.gl/qfbRgyVoiKLH1vxMA",
       },
     ],
@@ -24,18 +22,16 @@ const travelOptions = [
     origin: "Finland",
     methods: [
       {
-        icon: <FaPlane className="text-[#4A4238] mr-2" />,
-        description: "HKI to MUC (2,5h)",
+        description: "✈️ HKI to MUC (2,5h)",
         link: "https://www.google.com/travel/flights/s/6Hxh57QEeQEJfaTU6",
       },
       {
-        icon: <FaTrain className="text-[#4A4238] mr-2" />,
-        description: "MUC to Neuburg (ca. 1,5h)",
+        description: "🚂 MUC to Neuburg (ca. 1,5h)",
         link: "https://maps.app.goo.gl/9YiMe5BNR8eTEdGw7",
       },
       {
-        icon: <FaCar className="text-[#4A4238] mr-2" />,
-        description: "MUC to Neuburg (50min)",
+        icon: '',
+        description: "🚗 MUC to Neuburg (50min)",
         link: "https://maps.app.goo.gl/Ui5RGtTE5cgHsnv49",
       },
     ],
@@ -44,13 +40,11 @@ const travelOptions = [
     origin: "München",
     methods: [
       {
-        icon: <FaTrain className="text-[#4A4238] mr-2" />,
-        description: "München to Neuburg (ca. 1,5h)",
+        description: "🚂 München to Neuburg (ca. 1,5h)",
         link: "https://maps.app.goo.gl/1hGFLBeLyw2tLVWG7",
       },
       {
-        icon: <FaCar className="text-[#4A4238] mr-2" />,
-        description: "München to Neuburg (ca. 1h)",
+        description: "🚗 München to Neuburg (ca. 1h)",
         link: "https://maps.app.goo.gl/Ui5RGtTE5cgHsnv49",
       },
     ],
@@ -70,7 +64,6 @@ export const TravelInfo = () => (
                 key={methodIndex}
                 className="mb-2 text-[#4A4238] max-sm:text-base flex items-center justify-center"
               >
-                {method.icon}
                 {method.link ? (
                   <Link
                     href={method.link}
@@ -81,7 +74,7 @@ export const TravelInfo = () => (
                     <FaExternalLinkAlt className="ml-2 text-sm" />
                   </Link>
                 ) : (
-                  <span>{method.description}</span>
+                  <span>{` ${method.description} `}</span>
                 )}
               </li>
             ))}
