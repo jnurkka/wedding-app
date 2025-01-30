66a72c39d2f38061224caf533a02e4ff7d0f3508
Jaakko Nurkka <jaakko@cliniserve.de>
2025-01-30 21:33:14 +0100

    fix: scrolling behavior

--- src/app/components/SectionContainer.tsx
+++ src/app/components/SectionContainer.tsx
@@ -6,7 +6,7 @@ export const SectionContainer: React.FC<{
   bgImage?: string;
   id: string;
 }> = ({ id, children, bgColor, bgImage }) => {
-  const className = `section relative min-h-screen w-screen items-center snap-start flex flex-col justify-center px-6 md:px-12 lg:px-16 py-8`;
+  const className = `section relative min-h-screen w-screen items-center snap-start flex flex-col justify-center px-6 md:px-12 lg:px-16`;
   const style = {
     backgroundColor: bgColor,
     ...(bgImage
@@ -20,7 +20,9 @@ export const SectionContainer: React.FC<{
   return (
     <div id={id} className={className} style={style}>
       {bgImage && <Overlay />}
-      {children}
+      <div className="relative h-full w-full pt-8 pb-16">
+        {children}
+      </div>
     </div>
   );
 };
